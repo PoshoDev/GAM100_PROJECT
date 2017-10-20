76f5be29d42a189229f4851be1c434786e3e07c7
PoshoDev <32964490+PoshoDev@users.noreply.github.com>
2017-10-20 19:04:45 +0200

    Initial commit

---
+++ Variable List.c
@@ -0,0 +1,17 @@
+/*Here is where we will try to comment all of the objects and their variablesused in the game.
+Be organized and help each other not break their heads!!
+
+/// GLOBAL VARIABLES ///
+
+
+/// OBJECTS ///
+
+obj_char
+{
+    [Properties]
+    {
+
+    }
+}
+
+/*
